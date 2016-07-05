489cbc9f894eacf261c1bd81b1fc9ffc9c211fef
Gaute Solheim <gautesolheim@icloud.com>
2016-07-05 09:47:20 +0200

    Add pod w/ example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Gaute Solheim on 04.07.2016.
+//  Copyright © 2016 Gaute Solheim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
---
+++ FBSnapshotBase.h
@@ -0,0 +1,32 @@
+#define kIphone4PortraitRect CGRectMake(0, 0, 320, 480)
+#define kIphone5PortraitRect CGRectMake(0, 0, 320, 568)
+#define kIphone6PortraitRect CGRectMake(0, 0, 375, 667)
+#define kIphone6PlusPortraitRect CGRectMake(0, 0, 414, 736)
+#define kIphone4LandscapeRect CGRectMake(0, 0, 480, 320)
+#define kIphone5LandscapeRect CGRectMake(0, 0, 568, 320)
+#define kIphone6LandscapeRect CGRectMake(0, 0, 667, 375)
+#define kIphone6PlusLandscapeRect CGRectMake(0, 0, 736, 414)
+
+#define kIpadPortraitRect CGRectMake(0, 0, 768, 1024)
+#define kIpadLandscapeRect CGRectMake(0, 0, 1024, 768)
+#define kIpadMultitaskingLandscapeTwoToOneMainRect CGRectMake(0, 0, 694, 768)
+#define kIpadMultitaskingLandscapeTwoToOneAltRect CGRectMake(0, 0, 320, 768)
+#define kIpadMultitaskingLandscapeOneToOneMainAndAltRect CGRectMake(0, 0, 507, 768)
+#define kIpadMultitaskingPortraitOneToOneMainRect CGRectMake(0, 0, 438, 1024)
+#define kIpadMultitaskingPortraitOneToOneAltRect CGRectMake(0, 0, 320, 1024)
+
+@import FBSnapshotTestCase;
+//@import CommonSnapFramework
+
+//#import <FBSnapshotTestCase/FBSnapshotTestCase-umbrella.h>
+
+@interface FBSnapshotBase : FBSnapshotTestCase
+
+@property (nonatomic, strong) UIView *sut;
+@property (nonatomic, assign) BOOL recordAll;
+
+- (void)snapshotVerifyView:(UIView*)view;
+
+- (void)testIphone6Portrait;
+
+@end
